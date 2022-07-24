23aa69e789d6644bc95ffe71cad93a17276e1e4a
amishamalik23 <amishamalik23@gmail.com>
2022-07-24 16:04:36 +0530

    finalrepo

---
+++ jquery copy select/script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var text1= document.getElementById("inputuser");
+    text1.select();
+    text1.setSelectionRange(0,1000);
+    console.log(text1);
+    navigator.clipboard.writeText(text1.value);
+}
